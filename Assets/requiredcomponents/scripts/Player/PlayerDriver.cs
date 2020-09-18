d143b4bb0042409d5219fcdd09eda9a7ac46ec01
Siddu Eswar <siddueshu@gmail.com>
2020-09-18 23:37:05 +0530

    Input and movement completed

--- Assets/requiredcomponents/scripts/Player/PlayerDriver.cs
+++ Assets/requiredcomponents/scripts/Player/PlayerDriver.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InputManager))] // TODO: remove after testing
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerDriver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private A_InputHandler input;
+    private PlayerMotor motor;
+    //[Range(-1,1)]public float horizontalinput; // TODO: remove testing
+
+    private void Start()
     {
-        
+        input = GetComponent<A_InputHandler>();
+        motor = GetComponent<PlayerMotor>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        motor.Moveplayer(input.getinput());
     }
 }
---
+++ Assets/requiredcomponents/scripts/Player/PlayerMotor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController _character;
+    [SerializeField]private float sidelimit;
+    private Vector3 _position = Vector3.zero;
+
+    public float speed = 2; //TODO: Remove after power
+
+
+    private void Start()
+    {
+        _character = GetComponent<CharacterController>();
+        _character.detectCollisions = true;
+    }
+
+    public void Moveplayer(float horizontalvalue)
+    {
+        _position.x = Maprange(horizontalvalue)*speed;
+        _character.SimpleMove(_position);
+    }
+
+    private float Maprange(float inputvalue)
+    {
+        return inputvalue * sidelimit;
+    }
+}
