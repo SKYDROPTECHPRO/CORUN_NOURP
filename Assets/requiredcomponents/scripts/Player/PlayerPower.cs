aa3a7f7406a2cb4195f7e2c873626c56341cd910
Siddu Eswar <siddueshu@gmail.com>
2020-09-19 01:46:43 +0530

    Player power with movement and model

---
+++ Assets/requiredcomponents/scripts/Player/PlayerPower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPower : MonoBehaviour
+{
+    public float movespeed_hor { get; private set; }
+    [SerializeField] private List<S_PlayerModel> playermodels;
+    private List<GameObject> modelprefabs;
+    private int currentplayer=-1;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        modelprefabs=new List<GameObject>();
+        foreach (S_PlayerModel model in playermodels)
+        {
+            GameObject tempmodel = Instantiate(model.playermodel,transform);
+            tempmodel.SetActive(false);
+            modelprefabs.Add(tempmodel);
+        }
+    }
+
+    public void changeplayer(int modelnumber)
+    {
+        if (modelnumber != currentplayer && modelnumber<modelprefabs.Count)
+        {
+            if (currentplayer != -1) { modelprefabs[currentplayer].SetActive(false); }
+            currentplayer = modelnumber;
+            modelprefabs[currentplayer].SetActive(true);
+            gameObject.name = playermodels[currentplayer].modelname;
+            movespeed_hor = playermodels[currentplayer].movespeed_horizontal;
+        }
+    }
+}
---
+++ Assets/requiredcomponents/scripts/scriptable/S_PlayerModel.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Playermodel",menuName = "Scriptables/Playermodel")]
+public class S_PlayerModel : ScriptableObject
+{
+    public string modelname;
+    public float movespeed_horizontal;
+    public GameObject playermodel;
+}
